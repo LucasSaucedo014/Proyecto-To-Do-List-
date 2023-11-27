c632a95621fbc42648476593b4bc414b4a2f841f
LucasSaucedo014 <106402003+LucasSaucedo014@users.noreply.github.com>
2023-11-27 13:11:14 -0300

    Add files via upload

--- Lista de Tareas/src/App.jsx
+++ Lista de Tareas/src/App.jsx
@@ -5,21 +5,23 @@ import TaskForm from './components/TaskForm';
 import styled from 'styled-components';
 import './App.css';
 
+// Se utiliza styled-components para aplicar estilos al contenedor principal de la aplicación.
 const AppWrapper = styled.div`
   max-width: 800px;
   margin: 0 auto;
   text-align: center;
 `;
 
+// Definición del componente funcional principal de la aplicación.
 const App = () => {
-  const [tasks, setTasks] = useState([
-    // { id: 1, text: 'Task 1', completed: false, deleted: false },
-    // { id: 2, text: 'Task 2', completed: true, deleted: false },
-    // Agrega más tareas según sea necesario
-  ]);
+  // Estado para almacenar la lista de tareas. Inicialmente, la lista está vacía.
+  const [tasks, setTasks] = useState([]);
 
-  const [filter, setFilter] = useState('all'); // 'all', 'active', 'completed', 'deleted'
+  // Estado para gestionar el filtro actual aplicado a la lista de tareas.
+  // Puede tener los valores 'all', 'active', 'completed', 'deleted'.
+  const [filter, setFilter] = useState('all');
 
+  // Función para manejar el cambio de estado de completitud de una tarea.
   const handleToggleComplete = (taskId) => {
     setTasks((prevTasks) =>
       prevTasks.map((task) =>
@@ -28,17 +30,20 @@ const App = () => {
     );
   };
 
+  // Función para manejar la marcación de una tarea como eliminada.
   const handleDelete = (taskId) => {
     setTasks((prevTasks) =>
       prevTasks.map((task) => (task.id === taskId ? { ...task, deleted: true } : task))
     );
   };
 
+  // Función para añadir una nueva tarea a la lista.
   const handleAddTask = (taskText) => {
     const newTask = { id: tasks.length + 1, text: taskText, completed: false, deleted: false };
     setTasks((prevTasks) => [...prevTasks, newTask]);
   };
 
+  // Filtrar las tareas según el filtro actual.
   const filteredTasks = tasks.filter((task) => {
     if (filter === 'active') {
       return !task.completed && !task.deleted;
@@ -50,22 +55,27 @@ const App = () => {
     return !task.deleted;
   });
 
+  // Renderizar la interfaz de usuario de la aplicación.
   return (
     <AppWrapper>
       <h1 className="logo react">Lista de Tareas</h1>
+      {/* Componente para añadir nuevas tareas. */}
       <TaskForm onAddTask={handleAddTask} />
       <div className="card">
         <div>
+          {/* Botones para cambiar el filtro de visualización de tareas. */}
           Filtrar:
           <button onClick={() => setFilter('all')}>Todas</button>
           <button onClick={() => setFilter('active')}>Activas</button>
           <button onClick={() => setFilter('completed')}>Completadas</button>
           <button onClick={() => setFilter('deleted')}>Eliminadas</button>
         </div>
+        {/* Componente para mostrar la lista de tareas. */}
         <TaskList tasks={filteredTasks} onToggleComplete={handleToggleComplete} onDelete={handleDelete} />
       </div>
     </AppWrapper>
   );
 };
 
+// Exportar el componente principal de la aplicación.
 export default App;
